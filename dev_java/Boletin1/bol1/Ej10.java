c36ad18135546be0971c7c4371b1dfdb2b0b8db4
Mario Martin <147378155+mariomm2@users.noreply.github.com>
2026-01-13 10:10:35 +0100

    clase12enero

--- dev_java/Boletin1/bol1/Ej10.java
+++ dev_java/Boletin1/bol1/Ej10.java
@@ -1,5 +1,35 @@
 package bol1;
 
-public class Ej10 {
+import java.util.Scanner;
 
-}
+public class Ej10 {
+	    public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        int num = 0;
+	        int contador = 0;
+	        int sumaImpares = 0;
+	        int contImpares = 0;
+	        int mayorPar = -1;
+
+	        while (num >= 0) {
+	            System.out.print("Introduce un num: ");
+	            num = scan.nextInt();
+
+	            if (num >= 0) {
+	                contador++;
+	                if (num % 2 == 0) {
+	                    if (num > mayorPar) {
+	                        mayorPar = num;
+	                    }
+	                } else {
+	                    sumaImpares = sumaImpares + num;
+	                    contImpares++;
+	                }}}
+	        System.out.println("Total: " + contador);
+
+	        if (contImpares > 0) {
+	            System.out.println("Media impares: " + (sumaImpares/contImpares));
+	        }
+	        System.out.println("Mayor par: " + mayorPar);
+	      }}
+
